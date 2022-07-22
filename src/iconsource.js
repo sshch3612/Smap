be3cd952cc97046d5bcb85bcfafa3d55b85c060f
苏韶春 <susc@jh.com>
2022-07-22 17:23:11 +0800

    旋转

--- src/iconsource.js
+++ src/iconsource.js
@@ -216,4 +216,74 @@ export default function iconsource(Smap) {
         };
         _this.smap.addImage(id, gifImage); // 可作为icon-image使用
     }
+
+
+    Smap.prototype.loadDynamicRotateImg = function ({ id, size = 60, color = "55, 200, 200", imageObj = require("./assets/point.png"), scale = 1.0 }) {
+        const _this = this;
+        size = size * scale;
+        const gifImage = {
+            width: size,
+            height: size,
+            data: new Uint8Array(size * size * 4),
+            onAdd: function () {
+                console.log(arguments,"arguments")
+                const canvas = document.createElement('canvas');
+                canvas.width = this.width;
+                canvas.height = this.height;
+                this.context = canvas.getContext('2d');
+
+                this.img = new Image();
+                this.img.src = imageObj;
+                this.t = 1;
+                this.startAngle = 0;
+                //插入图片
+
+            },
+            render: function () {
+                const duration = 1000;
+                // const t = (performance.now() % duration) / duration;
+                // let t = 1;
+                if (this.t == 1) {
+                    console.log(this.context, 444);
+                }
+                this.t++;
+                // const radius = (size / 2) * .5;
+                // const outerRadius = (size / 2) * 0.4 + radius;
+                const context = this.context;
+
+                // // // draw outer circle
+                context.clearRect(0, 0, this.width, this.height);
+                // context.beginPath();
+                // context.arc(
+                //     this.width / 2,
+                //     this.height / 2,
+                //     radius,
+                //     0,
+                //     Math.PI * 2
+                // );
+                // context.strokeStyle = `rgba(${color},` + (1 - t) + ')';
+                // context.lineWidth = 2 + 10 * (1 - t);
+                // context.stroke();
+                context.translate(size / 2, size / 2);
+                context.rotate(Math.PI / 100)
+                context.translate(-size / 2, -size / 2);
+                context.drawImage(this.img, 0, 0, size, size);
+
+                // 
+                this.startAngle += .01;
+                // context.rotate(Math.PI / 2);
+
+                // update this image's data with data from the canvas
+                this.data = context.getImageData(
+                    0,
+                    0,
+                    this.width,
+                    this.height
+                ).data;
+                _this.smap.triggerRepaint()
+                return true
+            }
+        }
+        _this.smap.addImage(id, gifImage) // 可作为icon-image使用
+    }
 }
